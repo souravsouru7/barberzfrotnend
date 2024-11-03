a90a5b5e15a2b603a44d8959e4dcdfb802d69141
sourav R <128279891+souravsouru7@users.noreply.github.com>
2024-11-03 08:07:50 +0530

    Update adminSlice.js

--- src/redux/slices/adminSlice.js
+++ src/redux/slices/adminSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.barbezz.shop';
+axios.defaults.baseURL = 'https://www.barbezz.shop';
 
 
 const initialState = {
