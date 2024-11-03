673d0ea03f67a5527f932a32d882df7a070f9cde
sourav R <128279891+souravsouru7@users.noreply.github.com>
2024-11-03 08:08:11 +0530

    Update authSlice.js

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.barbezz.shop';
+axios.defaults.baseURL = 'https://www.barbezz.shop';
 
 export const registerUser = createAsyncThunk(
   'auth/registerUser',
